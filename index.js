779baa5b507597d2c436290020947d5c64e4f1ee
Harri <harri.kinsman@gmail.com>
2023-04-11 16:35:32 +0100

    Creating responsive navbar from scratch

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-let food = [];
-let totalAmount = 0;
+  let food = [];
+  let totalAmount = 0;
+  let menu = document.querySelector('#menu-icon');
+  let navbar = document.querySelector('.navbar');
+
+  menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+  }
 
   var scrollToTopBtn = $("#scrollToTop");
 
